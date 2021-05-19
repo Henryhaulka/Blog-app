bd02999b3bb27a40860de5cdbde01d25087b2f2f
henryhaulka <onuhenry24@gmail.com>
2021-05-19 15:21:06 +0100

    add a comment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
   root 'articles#index'
-  # get 'articles/show'
+  resources :articles do
+  resources :comments
+   # get 'articles/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
