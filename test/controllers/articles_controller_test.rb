715a4b9c3b2cea68308e2bd364de5c9a2dd5308e
henryhaulka <onuhenry24@gmail.com>
2021-06-04 17:39:27 +0100

    add test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
-
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get @articles_index_url
     assert_response :success
   end
+
+  test "should not save article without title" do
+  article = Article.new
+  assert_not article.save
+end
+
 end
