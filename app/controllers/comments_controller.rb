ed5e842315dac843cc251ddccfcb8d4c2ff7c43f
Henry Onu <onuhenry24@gmail.com>
2021-11-16 14:14:19 +0100

    Add status so form can save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.create!(comment_params)
     redirect_to article_path(@article)
   end
 
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 end
